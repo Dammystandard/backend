b55d42ee32ac1a71e5e761be186e220cfc6695ff
DAMMYSTANDARD <alukodammystandard2020@gmail.com>
2025-10-27 03:35:36 -0700

    backend

---
+++ app/__init__.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+from dotenv import load_dotenv
+from flask_jwt_extended import JWTManager
+import os
+
+load_dotenv()
+
+mongo = PyMongo()
+jwt = JWTManager()
+
+
+
+def create_app():
+    flask_app= Flask(__name__)
+
+
+    flask_app.config['MONGO_URI'] = os.getenv('MONGO_URI')
+    flask_app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+
+
+    @flask_app.route('/')
+    def hello_world():
+        return 'Hello, World!'
+
+    mongo.init_app(flask_app)
+    jwt.init_app(flask_app)
+
+    from app.auth import auth as auth_blueprint
+    flask_app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+    #check mongo db connection
+    try:
+        with flask_app.app_context():
+            mongo.db.command('ping')
+            print("Connected to MongoDB")
+    except Exception as e:
+        print("Error connecting to mongoDB:", e)        
+    
+    return flask_app
---
+++ app/auth.py
@@ -0,0 +1,45 @@
+from flask import Blueprint, request, jsonify
+from app import mongo
+import bcrypt
+from flask_jwt_extended import create_access_token
+from datetime import timedelta
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/signup', methods=['POST'])
+def signup():
+
+    try:
+        data = request.get_json()
+
+        if not data:
+            return jsonify({"msg": "Missing JSON in request"}), 400
+        
+        email = data.get('email')
+        password = data.get('password')
+        name = data.get('name')
+
+        if not email or not password:
+            return jsonify({"erroe": "Missing email or password"}), 401
+        
+        existing_user = mongo.db.users.find_one({"email": email})
+        if existing_user:
+            return jsonify({"msg": "user already exists"}),409
+        
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+        user_data = {
+            "email": email,
+            "password": hashed_password.decode('utf-8'),
+            "name": name
+        }
+        mongo.db.users.insert_one(user_data)
+
+        access_token = create_access_token(
+            identity=email,
+            expires_delta=timedelta(days=7)
+        )
+
+        return jsonify({"msg": "user created successfully", "access_token": access_token}), 201
+    except Exception as e:
+        return jsonify({"msg": "Error during signup", "error": str(e)}), 500
